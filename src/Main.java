57d4dd6ec95236c87aa06f72c8a91c04f0024336
gustavopurkoot <gustavopurkoot04053357@gmail.com>
2026-05-20 07:40:42 -0300

    refactor: extrair lógica do menu para métodos separados

--- src/Main.java
+++ src/Main.java
@@ -26,227 +26,23 @@ public class Main {
             switch (opcao) {
 
                 case 1:
-
-                    System.out.println("Digite o tipo do veículo (1 para Carro ou 2 para Caminhão): ");
-                    int tipoVeiculo = scanner.nextInt();
-                    scanner.nextLine();
-
-                    System.out.println("Digite a marca: ");
-                    String marca = scanner.nextLine();
-
-                    System.out.println("Digite o modelo: ");
-                    String modelo = scanner.nextLine();
-
-                    System.out.println("Digite o ano: ");
-                    int ano = scanner.nextInt();
-                    scanner.nextLine();
-
-                    System.out.println("Digite o nome do dono: ");
-                    String nomeDono = scanner.nextLine();
-
-                    System.out.println("Digite o CPF do dono: ");
-                    String cpfDono = scanner.nextLine();
-
-                    if (cpfDono.length() != 11) {
-                        System.out.println("Veículo não cadastrado devido ao CPF inválido.");
-                        break;
-
-                    }
-
-                    Dono dono = new Dono(nomeDono, cpfDono);
-
-                    if (tipoVeiculo == 1) {
-                        System.out.println("Digite o número de portas (2 ou 4): ");
-                        int portas = scanner.nextInt();
-                        scanner.nextLine();
-
-                        if (portas != 2 && portas != 4) {
-                            System.out.println("Número de portas inválido! Veículo não cadastrado.");
-                            break;
-                        }
-
-                        System.out.println("Digite o câmbio (Manual/Automático): ");
-                        String cambio = scanner.nextLine();
-                        if(!cambio.equalsIgnoreCase("Manual") && !cambio.equalsIgnoreCase("Automático")) {
-                            System.out.println("Câmbio inválido! Veículo não cadastrado.");
-                            break;
-                        }
-
-                        Carro carro = new Carro(marca, modelo, ano, dono, portas, cambio);
-
-                        if (carro.getAno() == 0) {
-                            System.out.println("Veículo não cadastrado devido ao ano inválido.");
-                            break;
-                        }
-
-                        veiculos.add(carro);
-                    } else if (tipoVeiculo == 2) {
-
-                        System.out.println("Digite a capacidade de carga em toneladas: ");
-                        double capacidadeCarga = scanner.nextDouble();
-                        scanner.nextLine();
-
-                        System.out.println("Digite o número de eixos: ");
-                        int eixos = scanner.nextInt();
-                        scanner.nextLine();
-
-                        Caminhao caminhao = new Caminhao(marca, modelo, ano, dono, capacidadeCarga, eixos);
-
-                        if (caminhao.getAno() == 0) {
-                            System.out.println("Veículo não cadastrado devido ao ano inválido.");
-                            break;
-                        }
-
-                        if (caminhao.getCapacidadeCarga() == 0) {
-                            System.out.println("Veículo não cadastrado devido à capacidade de carga inválida.");
-                            break;
-                        }
-
-                        if (caminhao.getEixos() == 0) {
-                            System.out.println("Veículo não cadastrado devido ao número de eixos inválido.");
-                            break;
-                        }
-
-                        veiculos.add(caminhao);
-                    } else {
-                        System.out.println("Tipo de veículo inválido! Veículo não cadastrado.");
-                        break;
-                    }
-
-                    System.out.println("\nVeículo cadastrado!");
+                    cadastrarVeiculo(scanner, veiculos);
                     break;
 
                 case 2:
-                    if (veiculos.isEmpty()) {
-                        System.out.println("\nNenhum veículo cadastrado.");
-                    } else {
-                        System.out.println(
-                                "Digite 1 para listar todos os veículos, 2 para listar por marca ou 3 para listar por cpf: ");
-                        int escolha = scanner.nextInt();
-                        scanner.nextLine();
-
-                        if (escolha == 1) {
-                            for (int i = 0; i < veiculos.size(); i++) {
-                                System.out.println("Veículo #" + (i + 1) + ":");
-                                veiculos.get(i).status();
-                                System.out.println("-----------------------------");
-
-                            }
-                            System.out.println("-----------------------------");
-
-                        } else if (escolha == 2) {
-                            System.out.println("Digite a marca que deseja listar: ");
-                            String marcaListar = scanner.nextLine();
-                            boolean encontrado = false;
-
-                            for (Veiculo veiculo : veiculos) {
-                                if (veiculo.getMarca().equalsIgnoreCase(marcaListar)) {
-                                    veiculo.status();
-                                    System.out.println("-----------------------------");
-                                    encontrado = true;
-                                }
-                            }
-                            if (!encontrado) {
-                                System.out.println("Nenhum veículo encontrado para a marca informada.");
-                            }
-                        } else if (escolha == 3) {
-                            System.out.println("Digite o CPF do dono que deseja listar: ");
-                            String cpflistar = scanner.nextLine();
-                            boolean encontradoCpf = false;
-
-                            for (Veiculo veiculo : veiculos) {
-                                if (veiculo.getDono().getCPF().equals(cpflistar)) {
-                                    veiculo.status();
-                                    System.out.println("-----------------------------");
-                                    encontradoCpf = true;
-                                }
-                            }
-                            if (!encontradoCpf) {
-                                System.out.println("Nenhum veículo encontrado para o CPF informado.");
-                            }
-
-                        } else {
-                            System.out.println("Opção inválida!");
-                        }
-                    }
+                    listarVeiculos(veiculos, scanner);
                     break;
 
                 case 3:
-                    if (veiculos.isEmpty()) {
-                        System.out.println("Nenhum veículo para remover.");
-                        break;
-                    }
-                    System.out.println("Digite o número do veículo que deseja remover: ");
-                    int numero = scanner.nextInt();
-                    scanner.nextLine();
-
-                    if (numero > 0 && numero <= veiculos.size()) {
-                        veiculos.remove(numero - 1);
-                        System.out.println("Veículo removido!");
-                    } else {
-                        System.out.println("Número inválido!");
-                    }
+                    removerVeiculo(scanner, veiculos);
                     break;
 
                 case 4:
-                    if (veiculos.isEmpty()) {
-                        System.out.println("Nenhum veiculo para atualizar.");
-                        break;
-                    }
-                    System.out.println("Digite o número do veículo que deseja atualizar: ");
-                    int num = scanner.nextInt();
-                    scanner.nextLine();
-
-                    if (num > 0 && num <= veiculos.size()) {
-
-                        Veiculo veiculoAtualizar = veiculos.get(num - 1);
-
-                        System.out.println("Digite a nova marca do veículo: ");
-                        String novaMarca = scanner.nextLine();
-
-                        System.out.println("Digite o novo modelo do veículo: ");
-                        String novoModelo = scanner.nextLine();
-
-                        System.out.println("Digite o novo ano do veículo: ");
-                        int novoAno = scanner.nextInt();
-                        scanner.nextLine();
-
-                        System.out.println("Digite o novo nome do dono do veículo: ");
-                        String novoNomeDono = scanner.nextLine();
-
-                        System.out.println("Digite o novo CPF do dono do veículo: ");
-                        String novoCpfDono = scanner.nextLine();
-
-                        if (!veiculoAtualizar.setAno(novoAno)) {
-                            System.out.println("Atualização cancelada.");
-                            break;
-                        }
-
-                        if (novoCpfDono.length() != 11) {
-                            System.out.println("CPF inválido! Atualização cancelada.");
-                            break;
-                        }
-
-                        veiculoAtualizar.setMarca(novaMarca);
-                        veiculoAtualizar.setModelo(novoModelo);
-                        veiculoAtualizar.getDono().setNome(novoNomeDono);
-                        veiculoAtualizar.getDono().setCPF(novoCpfDono);
-
-                        System.out.println("Veículo atualizado!");
-                    }
-
-                    else {
-                        System.out.println("Número inválido!");
-                    }
+                    atualizarVeiculo(scanner, veiculos);
                     break;
 
                 case 5:
-                    if (veiculos.isEmpty()) {
-                        System.out.println("Nenhum veículo para ordenar.");
-                    } else {
-                        Collections.sort(veiculos, (v1, v2) -> v1.getAno() - v2.getAno());
-                        System.out.println("Veículos ordenados!");
-                    }
+                    ordenarVeiculosPorAno(veiculos);
                     break;
 
                 case 0:
@@ -263,4 +59,236 @@ public class Main {
         System.out.println("Programa encerrado!");
         scanner.close();
     }
+
+    private static void cadastrarVeiculo(Scanner scanner, ArrayList<Veiculo> veiculos) {
+
+        System.out.println("Digite o tipo do veículo (1 para Carro ou 2 para Caminhão): ");
+        int tipoVeiculo = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite a marca: ");
+        String marca = scanner.nextLine();
+
+        System.out.println("Digite o modelo: ");
+        String modelo = scanner.nextLine();
+
+        System.out.println("Digite o ano: ");
+        int ano = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o nome do dono: ");
+        String nomeDono = scanner.nextLine();
+
+        System.out.println("Digite o CPF do dono: ");
+        String cpfDono = scanner.nextLine();
+
+        if (cpfDono.length() != 11) {
+            System.out.println("Veículo não cadastrado devido ao CPF inválido.");
+            return;
+
+        }
+
+        Dono dono = new Dono(nomeDono, cpfDono);
+
+        if (tipoVeiculo == 1) {
+            System.out.println("Digite o número de portas (2 ou 4): ");
+            int portas = scanner.nextInt();
+            scanner.nextLine();
+
+            if (portas != 2 && portas != 4) {
+                System.out.println("Número de portas inválido! Veículo não cadastrado.");
+                return;
+            }
+
+            System.out.println("Digite o câmbio (Manual/Automático): ");
+            String cambio = scanner.nextLine();
+            if (!cambio.equalsIgnoreCase("Manual") && !cambio.equalsIgnoreCase("Automático")) {
+                System.out.println("Câmbio inválido! Veículo não cadastrado.");
+                return;
+            }
+
+            Carro carro = new Carro(marca, modelo, ano, dono, portas, cambio);
+
+            if (carro.getAno() == 0) {
+                System.out.println("Veículo não cadastrado devido ao ano inválido.");
+                return;
+            }
+
+            veiculos.add(carro);
+        } else if (tipoVeiculo == 2) {
+
+            System.out.println("Digite a capacidade de carga em toneladas: ");
+            double capacidadeCarga = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.println("Digite o número de eixos: ");
+            int eixos = scanner.nextInt();
+            scanner.nextLine();
+
+            Caminhao caminhao = new Caminhao(marca, modelo, ano, dono, capacidadeCarga, eixos);
+
+            if (caminhao.getAno() == 0) {
+                System.out.println("Veículo não cadastrado devido ao ano inválido.");
+                return;
+            }
+
+            if (caminhao.getCapacidadeCarga() == 0) {
+                System.out.println("Veículo não cadastrado devido à capacidade de carga inválida.");
+                return;
+            }
+
+            if (caminhao.getEixos() == 0) {
+                System.out.println("Veículo não cadastrado devido ao número de eixos inválido.");
+                return;
+            }
+
+            veiculos.add(caminhao);
+        } else {
+            System.out.println("Tipo de veículo inválido! Veículo não cadastrado.");
+            return;
+        }
+
+        System.out.println("\nVeículo cadastrado!");
+        return;
+
+    }
+
+    private static void listarVeiculos(ArrayList<Veiculo> veiculos, Scanner scanner) {
+        if (veiculos.isEmpty()) {
+            System.out.println("\nNenhum veículo cadastrado.");
+        } else {
+            System.out.println(
+                    "Digite 1 para listar todos os veículos, 2 para listar por marca ou 3 para listar por cpf: ");
+            int escolha = scanner.nextInt();
+            scanner.nextLine();
+
+            if (escolha == 1) {
+                for (int i = 0; i < veiculos.size(); i++) {
+                    System.out.println("Veículo #" + (i + 1) + ":");
+                    veiculos.get(i).status();
+                    System.out.println("-----------------------------");
+
+                }
+                System.out.println("-----------------------------");
+
+            } else if (escolha == 2) {
+                System.out.println("Digite a marca que deseja listar: ");
+                String marcaListar = scanner.nextLine();
+                boolean encontrado = false;
+
+                for (Veiculo veiculo : veiculos) {
+                    if (veiculo.getMarca().equalsIgnoreCase(marcaListar)) {
+                        veiculo.status();
+                        System.out.println("-----------------------------");
+                        encontrado = true;
+                    }
+                }
+                if (!encontrado) {
+                    System.out.println("Nenhum veículo encontrado para a marca informada.");
+                }
+            } else if (escolha == 3) {
+                System.out.println("Digite o CPF do dono que deseja listar: ");
+                String cpflistar = scanner.nextLine();
+                boolean encontradoCpf = false;
+
+                for (Veiculo veiculo : veiculos) {
+                    if (veiculo.getDono().getCPF().equals(cpflistar)) {
+                        veiculo.status();
+                        System.out.println("-----------------------------");
+                        encontradoCpf = true;
+                    }
+                }
+                if (!encontradoCpf) {
+                    System.out.println("Nenhum veículo encontrado para o CPF informado.");
+                }
+
+            } else {
+                System.out.println("Opção inválida!");
+            }
+        }
+        return;
+    }
+
+    private static void removerVeiculo(Scanner scanner, ArrayList<Veiculo> veiculos) {
+        if (veiculos.isEmpty()) {
+            System.out.println("Nenhum veículo para remover.");
+            return;
+        }
+        System.out.println("Digite o número do veículo que deseja remover: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        if (numero > 0 && numero <= veiculos.size()) {
+            veiculos.remove(numero - 1);
+            System.out.println("Veículo removido!");
+        } else {
+            System.out.println("Número inválido!");
+        }
+        return;
+
+    }
+
+    private static void atualizarVeiculo(Scanner scanner, ArrayList<Veiculo> veiculos) {
+        if (veiculos.isEmpty()) {
+            System.out.println("Nenhum veiculo para atualizar.");
+            return;
+        }
+        System.out.println("Digite o número do veículo que deseja atualizar: ");
+        int num = scanner.nextInt();
+        scanner.nextLine();
+
+        if (num > 0 && num <= veiculos.size()) {
+
+            Veiculo veiculoAtualizar = veiculos.get(num - 1);
+
+            System.out.println("Digite a nova marca do veículo: ");
+            String novaMarca = scanner.nextLine();
+
+            System.out.println("Digite o novo modelo do veículo: ");
+            String novoModelo = scanner.nextLine();
+
+            System.out.println("Digite o novo ano do veículo: ");
+            int novoAno = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Digite o novo nome do dono do veículo: ");
+            String novoNomeDono = scanner.nextLine();
+
+            System.out.println("Digite o novo CPF do dono do veículo: ");
+            String novoCpfDono = scanner.nextLine();
+
+            if (!veiculoAtualizar.setAno(novoAno)) {
+                System.out.println("Atualização cancelada.");
+                return;
+            }
+
+            if (novoCpfDono.length() != 11) {
+                System.out.println("CPF inválido! Atualização cancelada.");
+                return;
+            }
+
+            veiculoAtualizar.setMarca(novaMarca);
+            veiculoAtualizar.setModelo(novoModelo);
+            veiculoAtualizar.getDono().setNome(novoNomeDono);
+            veiculoAtualizar.getDono().setCPF(novoCpfDono);
+
+            System.out.println("Veículo atualizado!");
+        }
+
+        else {
+            System.out.println("Número inválido!");
+        }
+        return;
+    }
+
+    private static void ordenarVeiculosPorAno(ArrayList<Veiculo> veiculos) {
+        if (veiculos.isEmpty()) {
+            System.out.println("Nenhum veículo para ordenar.");
+        } else {
+            Collections.sort(veiculos, (v1, v2) -> v1.getAno() - v2.getAno());
+            System.out.println("Veículos ordenados!");
+        }
+        return;
+    }
+
 }
